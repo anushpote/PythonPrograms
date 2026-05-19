64328a8cdb22bae97acd944864f8f98ca329382b
Anush Pote <anushpote345@gmail.com>
2026-05-19 10:46:43 +0545

    Exercise 1

---
+++ Exercise1.py
@@ -0,0 +1,9 @@
+# Exercise 1: Rectangle Area Calculation
+
+length = float(input("Enter the length: "))
+breadth = float(input("Enter the breadth: "))
+
+area = length * breadth
+
+print(f"The area is {area}cm²")
+
