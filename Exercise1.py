6130390554b359f9b08853528cc743ceb46e5cc0
Anush Pote <anushpote345@gmail.com>
2026-05-19 10:51:27 +0545

    First Exercises

--- Exercise1.py
+++ Exercise1.py
@@ -1,9 +1,18 @@
 # Exercise 1: Rectangle Area Calculation
 
-length = float(input("Enter the length: "))
-breadth = float(input("Enter the breadth: "))
+# length = float(input("Enter the length: "))
+# breadth = float(input("Enter the breadth: "))
 
-area = length * breadth
+# area = length * breadth
 
-print(f"The area is {area}cm²")
+# print(f"The area is {area}cm²")
 
+
+# Exercise 2: Shopping Cart Program
+
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is Rs.{total}")
