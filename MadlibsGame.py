9c5c83855c34b42577c9848c9e59281e1159aa2f
Anush Pote <anushpote345@gmail.com>
2026-05-19 11:02:22 +0545

    Madlibs Game

---
+++ MadlibsGame.py
@@ -0,0 +1,13 @@
+# Madlibs game
+# word game where you create a story by filling in blanks with random words
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun(person, place or thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb(an action) ending with 'ing': ")
+adjective3 = input("Enter an adjective (description):"\n)
+
+print(f"Today I went to a {adjective1} zoo.")   # an adjective is a description of something.(for ex: expensive, large, etc)
+print(f"In an exihibit, I saw a {noun1}.")  # a noun is a person, place, or thing. (for ex: a gorilla)
+print(f"{noun1} was {adjective2} and {verb1}")  # a verb is an action(for ex: running, eating, etc)
+print(f"I was {adjective3}!")
