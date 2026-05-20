8b88cab0f7eef5cd6bca391e2d32a137616c95c9
Anush Pote <anushpote345@gmail.com>
2026-05-20 12:30:59 +0545

    Area of the circle

--- Exercise2.py
+++ Exercise2.py
@@ -2,9 +2,17 @@
 
 import math
 
+# radius = float(input("Enter the radius of a circle: "))
+
+# circumference = (2 * math.pi * radius)
+# print(f"The circumference is: {round(circumference, 2)}cm")
+
+
+####################################################################
+# Area of a Circle ( pi * r²)
+
 radius = float(input("Enter the radius of a circle: "))
 
-circumference = (2 * math.pi * radius)
-print(f"The circumference is: {round(circumference, 2)}cm")
+area = math.pi * pow(radius, 2)
 
-
+print(f"The area of the circle is: {round(area, 2)}cm²")
