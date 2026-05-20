a1d207ca292e8493c24b5058aef841dd1c956d47
Anush Pote <anushpote345@gmail.com>
2026-05-20 12:22:03 +0545

    Math Functions

--- MathFunctions.py
+++ MathFunctions.py
@@ -1,11 +1,25 @@
-x= 3.4
-y = -4
-z = 5
+# x= 3.4
+# y = -4
+# z = 5
 
 #result = round(x)
 #result = abs(y)
 #result = pow(4,3)
 #result = max(x,y,z)
-result = min(x,y,z)
+#result = min(x,y,z)
+
+# print(result)
+
+###############################
+
+import math
+
+x= 9.9
+
+print(math.pi)
+print(math.e)   # e= exponential
+result = math.sqrt(x) # square root
+result = math.ceil(x)    # ceil rounds a floating point number up
+result = math.floor(x)    # floor rounds a floating point number down
 
 print(result)
